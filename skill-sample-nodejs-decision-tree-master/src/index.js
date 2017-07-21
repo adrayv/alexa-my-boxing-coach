c18fe9726a918834757042904f21145e6997a26a
adrayv <draym001@g.ucla.edu>
2017-07-20 19:22:07 -0700

    Changed conecpt of the skill. resolved STAGEMODE Unhandled Bug

--- skill-sample-nodejs-decision-tree-master/src/index.js
+++ skill-sample-nodejs-decision-tree-master/src/index.js
@@ -2,255 +2,190 @@ var Alexa = require('alexa-sdk');
 
 var states = {
     STARTMODE: '_STARTMODE',                // Prompt the user to start or restart the game.
-	PREWARMUP: '_PREWARMUP'
+    STAGEMODE: '_STAGEMODE'
 };
-
-// Punch Combinations
-var onePunches = [
-	"jab",
-	"cross",
-	"left hook",
-	"right hook",
-	"left uppercut",
-	"right uppercut",
-	"overhand"
-];
-
-var twoPunches = [
-	"double jab",
-	"double cross",
-	"jab cross",
-	"cross jab",
-	"left hook right hook",
-	"right hook left hook",
-	"cross hook",
-	"hook cross",
-	"cross uppercut",
-	"jab uppercut",
-	"jab left hook",
-	"jab right hook",
-	"jab overhand"
-];
-
-var threePunches = [
-	"triple jab",
-	"jab jab cross",
-	"jab cross jab",
-	"jab cross hook",
-	"jab uppercut hook",
-	"jab cross uppercut",
-	"cross hook cross",
-	"hook cross hook",
-	"hook cross uppercut",
-	"jab hook cross",
-	"uppercut hook cross",
-	"cross cross hook"
-];
-
-var fourPunches = [
-	"jab right hook left hook cross",
-	"jab right hook left hook uppercut",
-	"jab hook uppercut cross",
-	"jab uppercut jab cross",
-	"jab uppercut jab hook",
-	"jab uppercut hook cross",
-	"jab uppercut left hook right hook",
-	"jab uppercut hook uppercut",
-	"jab cross hook cross",
-	"jab cross left hook right hook",
-	"jab cross hook uppercut",
-	"jab cross uppercut cross",
-	"jab cross left uppercut right uppercut",
-	"jab hook jab cross",
-	"jab hook jab uppercut",
-	"jab cross jab cross",
-	"jab cross jab uppercut",
-	"jab cross hook overhand",
-	"jab overhand hook cross",
-	"jab overhand hook overhand"
-];
 
 
 // ---------------- Messages ----------------------
 
-var welcomeMessage = "Hi, I'm your Boxing Coach. For a great experince, It's best if you already have some knowledge of boxing, and that you have some open space near you before you start training. Would you like to continue?";
+var welcomeMsg = "Hi, I'm your Boxing Coach. ";
 
-var shortWelcome = "Would you like to continue?";
+var usageMsg = "We are going to practice our boxing skills through several rounds of punching combination drills. ";
 
-var sessionDescription = "In this session, we will do some warm-ups, practice our footwork, work on our defense, and refine our punches. For your safety, please train in an open space. Are you ready to begin?";
+var disclosureMsg = "These workouts are meant for users who already have some boxing experience to supplement the work they do at a real gym. For the best experience, use boxing gloves and a heavybag, or work mitts with a partner. ";
 
-var helpMessage = "We will train through a series of boxing exercies. For your safety, please train in an open-space environment. ";
+var promptNumRounds = "How many rounds of boxing would you like to do? ";
 
-var reprompt = "Sorry, I didn't get that. ";
+var workoutDesc1 = "Ok. We will do ";
 
-var goodbyeMessage = "Ok, See you next time!";
+var workoutDesc3 = "Each round will last for three minutes. You can use my commands to shadowbox, work on a heavybag, or work mitts with a partner. ";
 
-var beginWorkout = "Ok, Get ready. At the sound of the bell, we will begin. ";
+var continueMsg = "Would you like to continue? ";
+
+var readyMsg = "Are you ready to begin? ";
+
+//var helpMsg = "We will train through a series of boxing exercies. For your safety, please train in an open-space environment. ";
+
+// ---------------- Help Messages ----------------------
+
+var getHelp = "If you need help using this skill, whenever I ask you a question just say, I need help. ";
+
+var numRoundsHelp = "A typical round of boxing is three minutes. ";
+
+var numRoundsUsage = "Please say a number between one and twelve, and we will do that many number of rounds. ";
+
+var goodbyeMsg = "Ok, see you next time!";
 
 // ---------------- Variables ----------------------
 
-var warmupTime = "30 ";
+var numRounds = 0;
+var allowedRoundsLower = 1;
+var allowedRoundsUpper = 12;
 
 // ---------------- Sounds ----------------------
 
 var bell = "<audio src='https://s3.amazonaws.com/adrayv-bucket/alexa-boxing/boxing-bell.mp3'/> ";
+var breakQuick = "<break time='700ms'/> ";
+var break1 = "<break time='1s'/> ";
 var break3 = "<break time='3s'/> ";
 var break5 = "<break time='5s'/> ";
+var break30 = "<break time='10s'/> <break time='10s'/> <break time='10s'/> ";
 
 // ---------------- Transition Messages ----------------------
 
-var transitions = {
-	first: "First, ",
-	next: "<emphasis level='strong'>Great Job!</emphasis>. " + break3 + "Next, ",
-	lastly: "Lastly, "
-};
+var excitement = "Great! ";
+
+var praise = "<emphasis level='strong'>Well Done!</emphasis>. ";
+
+var reprompt = "Sorry, I didn't get that. ";
+
+var beginExercise = "Ok, Get ready. At the sound of the bell, we will begin. ";
+
+var catchBreath = "Take a moment to catch your breath. ";
 
 // ---------------- Modules ----------------------
 
-var warmups = [
-	"Let's do Jumping Jacks for " + warmupTime + "seconds. " + bell,
-	"Let's do High Knees for " + warmupTime + "seconds. " + bell,
-	"Let's do Torso Twists for " + warmupTime + "seconds. " + bell,
-	"Let's do Squats for " + warmupTime + "seconds. " + bell
-];
-
 // --------------- Handlers -----------------------
+
+// FIX: please fix all help and unhandled handlers. as of right now, they restore user back to start mode
 
 // Called when the session starts.
 exports.handler = function (event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(newSessionHandler, startGameHandlers, preWarmupHandler);
+    alexa.registerHandlers(newSessionHandler, startModeHandler, stagingHandler);
     alexa.execute();
 };
 
-// set state to start up and  welcome the user
+// set state to start up and welcome the user
 var newSessionHandler = {
-  'LaunchRequest': function () { 
-    this.handler.state = states.STARTMODE;
-    this.emit(':ask', welcomeMessage, reprompt + shortWelcome);
-  },
-  'AMAZON.HelpIntent': function () { // if a user does not know how to work it
-    this.handler.state = states.STARTMODE;
-    this.emit(':ask', helpMessage + shortWelcome, reprompt + helpMessage + shortWelcome);
-  },
-  'Unhandled': function () { // if a user does not say the right thing
-    this.emit(':ask', reprompt + shortWelcome, reprompt + shortWelcome); // CHECK FOR TYPO
-  }
+	'LaunchRequest': function () { 
+		var question = welcomeMsg + usageMsg + disclosureMsg + continueMsg;
+		var repeat = reprompt + getHelp + usageMsg + continueMsg;
+		this.handler.state = states.STARTMODE;
+		this.emit(':ask', question, repeat);
+	},
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMsg);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', goodbyeMsg);
+    },
+	'AMAZON.HelpIntent': function () { // if a user does not know how to work it
+		var question = usageMsg + continueMsg
+		var repeat = reprompt + question;
+		this.emit(':ask', question, repeat);
+	}
+	/*
+    'AMAZON.StartOverIntent': function () {
+		var greeting = welcomeMsg + usageMsg + disclosureMsg + continueMsg;
+		var repeat = reprompt + usageMsg + continueMsg;
+		this.emit(':ask', greeting, repeat);
+    }
+	'Unhandled': function () { // if a user puts an unrecognized statement
+		this.emit(':ask', reprompt + usageMsg + continueMsg); 
+	}
+	*/
 };
 
-// --------------- Functions that control the skill's behavior -----------------------
-
-// state before the workouts begin
-var startGameHandlers = Alexa.CreateStateHandler(states.STARTMODE, {
-    'AMAZON.YesIntent': function () {
-    	this.handler.state = states.PREWARMUP;
-    	this.emit(':ask', sessionDescription); // ask the session descirption, answered in WORKOUT mode
-    },
-    'AMAZON.NoIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.StartOverIntent': function () {
-        this.handler.state = states.STARTMODE;
-        this.emit(':ask', welcomeMessage, reprompt + shortWelcome);
-    },
-    'AMAZON.HelpIntent': function () {
-        this.handler.state = states.STARTMODE;
-		this.emit(':ask', helpMessage + shortWelcome, reprompt + helpMessage + shortWelcome);
-    },
-    'Unhandled': function () {
-		this.handler.state = states.STARTMODE;
-		this.emit(':ask', reprompt + shortWelcome);
-    }
-});
-
-var preWarmupHandler = Alexa.CreateStateHandler(states.PREWARMUP, {
-	'AMAZON.YesIntent': function () {
-		var warmupRoutine = buildWarmup(warmups);
-		this.emit(':tell', beginWorkout + warmupRoutine);
+var startModeHandler = Alexa.CreateStateHandler(states.STARTMODE, { // responses for after the welcome message
+	'AMAZON.YesIntent': function() {
+		var question = excitement + promptNumRounds;
+		var repeat = reprompt + getHelp + promptNumRounds;
+		this.handler.state = states.STAGEMODE;
+		this.emit(':ask', question, repeat);
 	},
-    'AMAZON.NoIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
+	'AMAZON.NoIntent': function() {
+		this.emit(':tell', goodbyeMsg);
+	},
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', goodbyeMessage);
+        this.emit(':tell', goodbyeMsg);
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', goodbyeMessage);
+        this.emit(':tell', goodbyeMsg);
     },
     'AMAZON.StartOverIntent': function () {
-        this.handler.state = states.STARTMODE;
-        this.emit(':ask', welcomeMessage, reprompt + shortWelcome);
+		var question = welcomeMsg + usageMsg + disclosureMsg + continueMsg;
+		var repeat = reprompt + getHelp + usageMsg + continueMsg;
+		this.emit(':ask', question, repeat);
     },
-    'AMAZON.HelpIntent': function () {
-        this.handler.state = states.STARTMODE;
-		this.emit(':ask', helpMessage + shortWelcome, reprompt + helpMessage + shortWelcome);
+	'AMAZON.HelpIntent': function () { // if a user does not know how to work it from the welcomeMessage
+		var question = usageMsg + continueMsg
+		var repeat = reprompt + question;
+		this.emit(':ask', question, repeat);
+	},
+	'Unhandled': function () { // handles unrecognized phrases from welomeMessage 
+		var question = reprompt + getHelp + continueMsg;
+		this.emit(':ask', question); 
+	}
+});
+
+var stagingHandler = Alexa.CreateStateHandler(states.STAGEMODE, { // responses for after the welcome message
+	'numRoundsIntent': function() {
+		var question = undefined;
+		var repeat = undefined;
+		var slotVal = undefined;
+		slotVal = this.event.request.intent.slots.NUMROUNDS.value;
+		if(slotVal) {
+			numRounds = slotVal;
+			if(numRounds < allowedRoundsLower || numRounds > allowedRoundsUpper) {
+				question = numRoundsUsage + promptNumRounds;
+				repeat = reprompt + question;
+				this.emit(':ask', question, repeat);
+			}
+			var question = getWorkoutDesc(numRounds);
+			var repeat = reprompt + readyMsg;
+			this.emit(':tell', question, repeat);
+			//this.handler.state = states.
+		}
+		else { // called if a user does not give a numRoundsIntent.
+			var question = reprompt + getHelp + numRoundsUsage + promptNumRounds;
+			this.emit(':ask', question);
+		}
+	},
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', goodbyeMsg);
     },
-    'Unhandled': function () {
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', goodbyeMsg);
+    },
+    'AMAZON.StartOverIntent': function () {
+		var question = welcomeMsg + usageMsg + disclosureMsg + continueMsg;
+		var repeat = reprompt + getHelp + usageMsg + continueMsg;
 		this.handler.state = states.STARTMODE;
-		this.emit(':ask', reprompt + shortWelcome);
-    }
+		this.emit(':ask', question, repeat);
+    },
+	'AMAZON.HelpIntent': function () {
+		var question = usageMsg + numRoundsHelp + numRoundsUsage + promptNumRounds;
+		var repeat =  reprompt + promptNumRounds;
+		this.emit(':ask', question, repeat);
+	},
+	'Unhandled': function () { // This is not called when getting an Unhandled utterance in this state
+		var question = reprompt + getHelp + numRoundsUsage + promptNumRounds;
+		this.emit(':ask', question);
+	}
 });
-
-
-/*
-// user will have been asked a question when this intent is called. We want to look at their yes/no
-// response and then ask another question. If we have asked more than the requested number of questions Alexa will
-// make a choice, inform the user and then ask if they want to play again
-var askQuestionHandlers = Alexa.CreateStateHandler(states.ASKMODE, {
-
-    'AMAZON.YesIntent': function () {
-        this.handler.state = states.ASKMODE;
-        var message = getSpeech();
-        this.emit(':ask', message, repromptPunch + message);
-    },
-    'AMAZON.NoIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', promptToSayYesNo, reprompt + promptToSayYesNo);
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', goodbyeMessage);
-    },
-    'AMAZON.StartOverIntent': function () {
-        // reset the game state to start mode
-        this.handler.state = states.STARTMODE;
-        this.emit(':ask', welcomeMessage, reprompt + repeatWelcomeMessage);
-    },
-    'Unhandled': function () {
-        this.emit(':ask', promptToSayYesNo, reprompt + promptToSayYesNo);
-    }
-});
-*/
 
 // --------------- Helper Functions  -----------------------
-
-function getSpeech() {
-    var nodeIndex = Math.floor(Math.random() * nodes.length);
-    return nodes[nodeIndex];
-}
-
-function buildWarmup(unshuffled) {
-    warmups = shuffle(unshuffled);
-    var str = transitions['first'] + warmups[0] + break5; // set up the first warmup
-    for(i = 1; i < warmups.length - 1; ++i) { // iterate through all but the last warmup
-		str += transitions['next'];
-		str += warmups[i];
-		str += break5; // FIX: chage this to a longer time
-    }
-    str += transitions['lastly'];
-    str += warmups[warmups.length - 1];
-    return str;
-}
 
 function shuffle(a) {
     var j, x, i;
@@ -261,4 +196,15 @@ function shuffle(a) {
     	a[j] = x;
 	}
 	return a;
+}
+
+function getWorkoutDesc(num) {
+	var workoutDesc2 = undefined;
+	if(num == 1) {
+		workoutDesc2 = " round. ";
+	}
+	else {
+		workoutDesc2 = " rounds. ";
+	}
+	return workoutDesc1 + num.toString() + workoutDesc2 + workoutDesc3 + readyMsg;
 }
